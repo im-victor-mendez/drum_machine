935a71dfd2d9702056b6e395c4431d38c3334421
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-05-04 19:49:26 -0600

    feat: Implemented key press functionality

--- src/components/DrumPad/DrumPad.jsx
+++ src/components/DrumPad/DrumPad.jsx
@@ -1,6 +1,30 @@
+import { useCallback, useEffect } from "react"
+
 function DrumPad({ name, keyboard, audioImport }) {
+    const handleKeyPress = useCallback(keyPress, [keyboard, name])
+
+    function keyPress(event) {
+        const { key } = event
+        const drumPad = document.getElementById(name)
+        
+        if (key.toUpperCase() === keyboard) drumPad.click()
+    }
+
+    useEffect(() => {
+      document.addEventListener('keypress', handleKeyPress)
+    
+      return () => {
+        document.removeEventListener('keypress', handleKeyPress)
+      }
+    }, [handleKeyPress])
+
+    function playAudio() {
+        const audio = document.getElementById(keyboard)
+        audio.play()
+    }
+
     return (
-        <button id={name} className="drum-pad">
+        <button id={name} className="drum-pad" onClick={playAudio}>
             <audio id={keyboard} className="clip" src={audioImport}></audio>
             {keyboard}
         </button>
